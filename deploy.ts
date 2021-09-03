6b5b388d372701c572dfe064833a1cf1f118331a
iccee0 <rouven@12i.ch>
2021-09-03 10:34:02 +0200

    d

--- deploy.ts
+++ deploy.ts
@@ -1,9 +1,19 @@
 import { MongoProject, MongoDenoExecutable, MongoDenoModule } from "https://www.roeh.ch/x/roehMongo";
 
-addEventListener("fetch", (event) => {
-  const x = "j";
-  const response = new Response(x, {
-    headers: { "content-type": "application/json" },
-  });
-  event.respondWith(response);
-});
+export function handleRequest(request: { url: string; }) {
+  const { pathname } = new URL(request.url);
+
+  // Respond with JSON
+  if (pathname.startsWith("/json")) {
+    // Use stringify function to convert javascript object to JSON string.
+    const json = JSON.stringify({
+      message: "Hello from Deno Deploy",
+    });
+
+    return new Response(json, {
+      headers: {
+        "content-type": "application/json; charset=UTF-8",
+      },
+    });
+  }
+}
