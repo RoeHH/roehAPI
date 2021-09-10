ad7d651e5417881aeda695efc302b5e4630e28e5
iccee0 <rouven@12i.ch>
2021-09-10 12:21:12 +0200

    g

--- mongo.ts
+++ mongo.ts
@@ -1,6 +1,6 @@
 import { MongoDenoExecutable, denoExecutable } from "https://gitlab.wuersch.org/iccee0/roeh-cli/-/raw/main/mongo.ts";
 
-async function getExecutable(): Promise<denoExecutable[]> {
+export async function getExecutable(): Promise<denoExecutable[]> {
   return await MongoDenoExecutable.find(undefined, {
     noCursorTimeout: false,
   }as any).toArray();
